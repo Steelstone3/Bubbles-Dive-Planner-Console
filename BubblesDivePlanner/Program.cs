ebdbed3bb753bc8c543e714e421e8e75d9f7712b
Skippy <steelstone3@gmail.com>
2022-11-19 13:02:34 +0000

    Refactored into one service that runs the application

--- BubblesDivePlanner/Program.cs
+++ BubblesDivePlanner/Program.cs
@@ -8,21 +8,14 @@ namespace BubblesDivePlanner
     {
         static void Main(string[] args)
         {
-            // TODO extract to some sort of runner service
-            var presenter = new Presenter();
-            var diveSetupPresenter = new DiveSetupPresenter(presenter);
-            var diveStepPresenter = new DiveStepPresenter(presenter);
-            var diveStagesController = new DiveStagesController();
-            var diveController = new DiveController(diveStepPresenter, diveSetupPresenter, diveStagesController);
-
-            diveController.SetupDivePlan();
-
-            do
-            {
-                var divePlan = diveController.SetupDiveStep();
-                var updatedDivePlan = diveController.RunDiveProfile(divePlan);
-                diveController.PrintDiveResults(updatedDivePlan.DiveModel.DiveProfile);
-            } while (presenter.GetConfirmation("Continue?"));
+            IPresenter presenter = new Presenter();
+            IDiveSetupPresenter diveSetupPresenter = new DiveSetupPresenter(presenter);
+            IDiveStepPresenter diveStepPresenter = new DiveStepPresenter(presenter);
+            IDiveStagesController diveStagesController = new DiveStagesController();
+            IDiveController diveController = new DiveController(diveStepPresenter, diveSetupPresenter, diveStagesController);
+            IDivePlannerService divePlannerService = new DivePlannerService(diveController);
+            
+            divePlannerService.Run(presenter);
         }
     }
 }
