dcfbdb717ae7ac31d0740cc5af4e47cd4eafc80d
Skippy <steelstone3@gmail.com>
2022-11-19 12:51:58 +0000

    Basic dive service added

--- BubblesDivePlanner/Services/DivePlannerService.cs
+++ BubblesDivePlanner/Services/DivePlannerService.cs
@@ -1,10 +1,22 @@
+using BubblesDivePlanner.Controllers;
+
 namespace BubblesDivePlannerTests.Services
 {
     public class DivePlannerService : IDivePlannerService
     {
+        private readonly IDiveController diveController;
+
+        public DivePlannerService(IDiveController diveController)
+        {
+            this.diveController = diveController;
+        }
+
         public void Run()
         {
-            throw new System.NotImplementedException();
+            diveController.SetupDivePlan();
+            var divePlan = diveController.SetupDiveStep();
+            divePlan = diveController.RunDiveProfile(divePlan);
+            diveController.PrintDiveResults(divePlan.DiveModel.DiveProfile);
         }
     }
 }
--- BubblesDivePlannerTests/Services/DivePlannerServiceShould.cs
+++ BubblesDivePlannerTests/Services/DivePlannerServiceShould.cs
@@ -9,19 +9,20 @@ namespace BubblesDivePlannerTests.Services
 {
     public class DivePlannerServiceShould
     {
-        [Fact(Skip = "To do next")]
+        [Fact]
         public void RunDivePlannerService()
         {
             // Given
             var divePlan = new Mock<IDivePlan>();
-            var diveProfile = new Mock<IDiveProfile>();
+            divePlan.Setup(dp => dp.DiveModel).Returns(TestFixture.FixtureDiveModel);
+            divePlan.Setup(dp => dp.DiveModel.DiveProfile).Returns(TestFixture.FixtureDiveModel.DiveProfile);
             var diveController = new Mock<IDiveController>();
             diveController.Setup(dc => dc.SetupDivePlan());
-            diveController.Setup(dc => dc.SetupDiveStep());
-            diveController.Setup(dc => dc.RunDiveProfile(divePlan.Object));
-            diveController.Setup(dc => dc.PrintDiveResults(diveProfile.Object));
+            diveController.Setup(dc => dc.SetupDiveStep()).Returns(divePlan.Object);
+            diveController.Setup(dc => dc.RunDiveProfile(divePlan.Object)).Returns(divePlan.Object);
+            diveController.Setup(dc => dc.PrintDiveResults(divePlan.Object.DiveModel.DiveProfile));
             var diveStagesController = new Mock<IDiveStagesController>();
-            IDivePlannerService divePlannerService = new DivePlannerService();
+            IDivePlannerService divePlannerService = new DivePlannerService(diveController.Object);
             
             // When
             divePlannerService.Run();
