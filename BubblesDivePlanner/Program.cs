6c0180895ca7798f2d1059bd6233730e9073efc6
Skippy <steelstone3@gmail.com>
2022-11-19 01:52:58 +0000

    Can extract to some sort of runner service that is testable

--- BubblesDivePlanner/Program.cs
+++ BubblesDivePlanner/Program.cs
@@ -1,10 +1,28 @@
+using BubblesDivePlanner.Controllers;
+using BubblesDivePlanner.Presenters;
+using BubblesDivePlannerTests.Services;
+
 namespace BubblesDivePlanner
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // TODO extract to some sort of runner service
+            var presenter = new Presenter();
+            var diveSetupPresenter = new DiveSetupPresenter(presenter);
+            var diveStepPresenter = new DiveStepPresenter(presenter);
+            var diveController = new DiveController();
+            var diveService = new DiveService(diveStepPresenter, diveSetupPresenter, diveController);
 
+            diveService.SetupDivePlan();
+
+            do
+            {
+                var divePlan = diveService.SetupDiveStep();
+                var updatedDivePlan = diveService.RunDiveProfile(divePlan);
+                diveService.PrintDiveResults(updatedDivePlan.DiveModel.DiveProfile);
+            } while (presenter.GetConfirmation("Continue?"));
         }
     }
 }
