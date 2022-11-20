7e4689d626597bf93e5bf33d7ace45132790fe1e
Skippy <steelstone3@gmail.com>
2022-11-20 15:17:14 +0000

    Added an acceptance test for File Controller

--- BubblesDivePlannerTests/Controllers/Json/FileControllerShould.cs
+++ BubblesDivePlannerTests/Controllers/Json/FileControllerShould.cs
@@ -59,5 +59,41 @@ namespace BubblesDivePlannerTests.Controllers.Json
             // Then
             presenter.VerifyAll();
         }
+
+        [Fact]
+        public void AcceptanceTest()
+        {
+            // Given
+            var expectedDivePlan = new DivePlan(TestFixture.FixtureDiveModel, TestFixture.FixtureCylinders(), TestFixture.FixtureDiveStep, TestFixture.FixtureSelectedCylinder);
+            presenter.Setup(p => p.GetConfirmation("Save File?")).Returns(true);
+            presenter.Setup(p => p.GetConfirmation("Load File?")).Returns(true);
+            fileController = new FileController(presenter.Object);
+
+            // When
+            fileController.SaveFile(expectedDivePlan);
+            var divePlan = fileController.LoadFile();
+
+            // Then
+            presenter.VerifyAll();
+            Assert.Equal(expectedDivePlan.DiveModel.DiveProfile.OxygenPressureAtDepth, divePlan.DiveModel.DiveProfile.OxygenPressureAtDepth);
+            Assert.Equal(expectedDivePlan.DiveModel.DiveProfile.HeliumPressureAtDepth, divePlan.DiveModel.DiveProfile.HeliumPressureAtDepth);
+            Assert.Equal(expectedDivePlan.DiveModel.DiveProfile.NitrogenPressureAtDepth, divePlan.DiveModel.DiveProfile.NitrogenPressureAtDepth);
+            Assert.Equal(expectedDivePlan.DiveModel.DiveProfile.NitrogenTissuePressures, divePlan.DiveModel.DiveProfile.NitrogenTissuePressures);
+            Assert.Equal(expectedDivePlan.DiveModel.DiveProfile.HeliumTissuePressures, divePlan.DiveModel.DiveProfile.HeliumTissuePressures);
+            Assert.Equal(expectedDivePlan.DiveModel.DiveProfile.TotalTissuePressures, divePlan.DiveModel.DiveProfile.TotalTissuePressures);
+            Assert.Equal(expectedDivePlan.DiveModel.DiveProfile.MaxSurfacePressures, divePlan.DiveModel.DiveProfile.MaxSurfacePressures);
+            Assert.Equal(expectedDivePlan.DiveModel.DiveProfile.ToleratedAmbientPressures, divePlan.DiveModel.DiveProfile.ToleratedAmbientPressures);
+            Assert.Equal(expectedDivePlan.DiveModel.DiveProfile.CompartmentLoads, divePlan.DiveModel.DiveProfile.CompartmentLoads);
+
+            for (int i = 0; i < expectedDivePlan.Cylinders.Count; i++)
+            {
+                Assert.Equal(expectedDivePlan.Cylinders[i].CylinderPressure, divePlan.Cylinders[i].CylinderPressure);
+                Assert.Equal(expectedDivePlan.Cylinders[i].CylinderVolume, divePlan.Cylinders[i].CylinderVolume);
+                Assert.Equal(expectedDivePlan.Cylinders[i].InitialPressurisedVolume, divePlan.Cylinders[i].InitialPressurisedVolume);
+                Assert.Equal(expectedDivePlan.Cylinders[i].RemainingGas, divePlan.Cylinders[i].RemainingGas);
+                Assert.Equal(expectedDivePlan.Cylinders[i].SurfaceAirConsumptionRate, divePlan.Cylinders[i].SurfaceAirConsumptionRate);
+                Assert.Equal(expectedDivePlan.Cylinders[i].UsedGas, divePlan.Cylinders[i].UsedGas);
+            }
+        }
     }
 }
