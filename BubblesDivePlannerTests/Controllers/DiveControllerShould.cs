2b20276c666dca4d36af82a11207601046626ee6
Skippy <steelstone3@gmail.com>
2022-11-20 23:23:38 +0000

    Update DiveControllerShould.cs

--- BubblesDivePlannerTests/Controllers/DiveControllerShould.cs
+++ BubblesDivePlannerTests/Controllers/DiveControllerShould.cs
@@ -137,36 +137,5 @@ namespace BubblesDivePlannerTests.Controllers
             // Then
             diveSetupPresenter.VerifyAll();
         }
-
-        // [Fact]
-        // public void RunGasManagement()
-        // {
-        //     // Given
-        //     var diveStep = new Mock<IDiveStep>();
-        //     var selectedCylinder = new Mock<ICylinder>();
-        //     selectedCylinder.Setup(sc => sc.UpdateCylinderGasConsumption(diveStep.Object));
-        //     diveController = new DiveController(diveStepPresenter.Object, diveSetupPresenter.Object, diveStagesController.Object);
-
-        //     // When
-        //     diveController.RunGasManagement(selectedCylinder.Object, diveStep.Object);
-
-        //     // Then
-        //     selectedCylinder.VerifyAll();
-        // }
-
-        // [Fact]
-        // public void PrintCylinder()
-        // {
-        //     // Given
-        //     var selectedCylinder = new Mock<ICylinder>();
-        //     diveSetupPresenter.Setup(dsp => dsp.PrintCylinder(selectedCylinder.Object));
-        //     diveController = new DiveController(diveStepPresenter.Object, diveSetupPresenter.Object, diveStagesController.Object);
-
-        //     // When
-        //     diveController.PrintCylinder(selectedCylinder.Object);
-
-        //     // Then
-        //     diveSetupPresenter.VerifyAll();
-        // }
     }
 }
