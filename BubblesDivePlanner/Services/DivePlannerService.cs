5c1189a3de7f25882542d23ae60380d9928ab050
Skippy <steelstone3@gmail.com>
2022-11-21 18:11:28 +0000

    Creates json file with only one of what should be many states for dive steps, dive profiles, cylinders and selected cylinders

--- BubblesDivePlanner/Services/DivePlannerService.cs
+++ BubblesDivePlanner/Services/DivePlannerService.cs
@@ -29,7 +29,7 @@ namespace BubblesDivePlannerTests.Services
                 divePlan = diveController.RunDiveProfile(divePlan);
                 diveController.PrintDiveResults(divePlan);
                 divePlans.Add(divePlan);
-                // divePlan.ApplicationState.UpdateApplicationState(divePlan);
+                divePlan.DivePlanner.UpdateDivePlans(divePlan);
             } while (presenter.GetConfirmation("Continue?"));
 
             // put divePlans in here
