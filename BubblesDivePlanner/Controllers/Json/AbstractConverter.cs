fae870c4f6d7686caee97ce41ebe53d1c83d10a5
Skippy <steelstone3@gmail.com>
2022-11-26 13:07:42 +0000

    This as an idea but it doesn't work

--- BubblesDivePlanner/Controllers/Json/AbstractConverter.cs
+++ BubblesDivePlanner/Controllers/Json/AbstractConverter.cs
@@ -1,4 +1,5 @@
 using System;
+using BubblesDivePlanner.Models.DiveModels;
 using Newtonsoft.Json;
 
 namespace BubblesDivePlanner.Controllers.Json
@@ -9,7 +10,22 @@ namespace BubblesDivePlanner.Controllers.Json
             => objectType == typeof(TAbstract);
 
         public override Object ReadJson(JsonReader reader, Type type, Object value, JsonSerializer jsonSerialiser)
-            => jsonSerialiser.Deserialize<TReal>(reader);
+        {
+            if (value.GetType() == typeof(FakeUsnRev6))
+            {
+                return jsonSerialiser.Deserialize<FakeUsnRev6>(reader);
+            }
+            else if (value.GetType() == typeof(FakeZhl12Buhlmann))
+            {
+                return jsonSerialiser.Deserialize<FakeZhl12Buhlmann>(reader);
+            }
+            else if (value.GetType() == typeof(Zhl16Buhlmann))
+            {
+                return jsonSerialiser.Deserialize<Zhl16Buhlmann>(reader);
+            }
+
+            return jsonSerialiser.Deserialize<TReal>(reader);
+        }
 
         public override void WriteJson(JsonWriter writer, Object value, JsonSerializer jsonSerialiser)
             => jsonSerialiser.Serialize(writer, value);
