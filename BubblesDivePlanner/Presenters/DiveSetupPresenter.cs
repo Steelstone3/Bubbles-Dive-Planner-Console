6338844ca299f8b1df77db2f84fd1e0f42be138c
Skippy <steelstone3@gmail.com>
2022-11-26 11:58:56 +0000

    Added a fake dive model to check it loads and saves correctly

--- BubblesDivePlanner/Presenters/DiveSetupPresenter.cs
+++ BubblesDivePlanner/Presenters/DiveSetupPresenter.cs
@@ -24,13 +24,17 @@ namespace BubblesDivePlanner.Presenters
 
         public IDiveModel SelectDiveModel()
         {
+            List<IDiveModel> diveModels = new List<IDiveModel>()
+            {
+                new Zhl16Buhlmann(null),
+                new FakeZhl12Buhlmann(null),
+            };
+
             var selectionPrompt = new SelectionPrompt<IDiveModel> { Converter = diveModel => diveModel.Name };
 
             return AnsiConsole.Prompt(selectionPrompt
             .Title("Select Dive Model:")
-            .AddChoices(new[] {
-                new Zhl16Buhlmann(null)
-            }));
+            .AddChoices(diveModels));
         }
 
         public List<ICylinder> CreateCylinders()
@@ -110,8 +114,8 @@ namespace BubblesDivePlanner.Presenters
             foreach (var cylinder in cylinders)
             {
                 var row = new[] {
-                    cylinder.Name, 
-                    cylinder.InitialPressurisedVolume.ToString(), 
+                    cylinder.Name,
+                    cylinder.InitialPressurisedVolume.ToString(),
                     cylinder.RemainingGas.ToString(),
                     cylinder.UsedGas.ToString(),
                     cylinder.GasMixture.Oxygen.ToString(),
