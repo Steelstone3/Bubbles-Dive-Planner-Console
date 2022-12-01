38fd1eb89a4e5b95e4c346a1a5fd360de1d159ad
Skippy <steelstone3@gmail.com>
2022-12-01 14:50:28 +0000

    Added dive model to user interface

--- BubblesDivePlanner/Presenters/DiveSetupPresenter.cs
+++ BubblesDivePlanner/Presenters/DiveSetupPresenter.cs
@@ -30,6 +30,7 @@ namespace BubblesDivePlanner.Presenters
                 new Zhl16Buhlmann(null),
                 new Zhl12Buhlmann(null),
                 new UsnRevision6(null),
+                new DcapMf11f6(null),
             };
 
             var selectionPrompt = new SelectionPrompt<IDiveModel> { Converter = diveModel => diveModel.Name };
