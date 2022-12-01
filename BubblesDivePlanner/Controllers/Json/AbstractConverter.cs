71758c5bd06b1031a686d620588c05fa707660f8
Skippy <steelstone3@gmail.com>
2022-12-01 01:24:36 +0000

    Need to get dive model name from the file

--- BubblesDivePlanner/Controllers/Json/AbstractConverter.cs
+++ BubblesDivePlanner/Controllers/Json/AbstractConverter.cs
@@ -1,5 +1,4 @@
 using System;
-using BubblesDivePlanner.Models.DiveModels;
 using Newtonsoft.Json;
 
 namespace BubblesDivePlanner.Controllers.Json
@@ -9,20 +8,8 @@ namespace BubblesDivePlanner.Controllers.Json
         public override Boolean CanConvert(Type objectType)
             => objectType == typeof(TAbstract);
 
-        public override Object ReadJson(JsonReader reader, Type type, Object value, JsonSerializer jsonSerialiser)
-        {
-            if (type == typeof(DiveModel))
-            {
-                // TODO Read the dive model name from the file then return the casted type based on the name
-                var bob = value as DiveModel;
-                
-                if(bob.Name == DiveModelNames.ZHL16.ToString()) {
-                    return jsonSerialiser.Deserialize<Zhl16Buhlmann>(reader);
-                }
-            }
-
-            return jsonSerialiser.Deserialize<TReal>(reader);
-        }
+        public override Object ReadJson(JsonReader reader, Type type, Object value, JsonSerializer jsonSerialiser) 
+            => jsonSerialiser.Deserialize<TReal>(reader);
 
         public override void WriteJson(JsonWriter writer, Object value, JsonSerializer jsonSerialiser)
             => jsonSerialiser.Serialize(writer, value);
--- BubblesDivePlanner/Controllers/Json/JsonController.cs
+++ BubblesDivePlanner/Controllers/Json/JsonController.cs
@@ -4,6 +4,7 @@ using BubblesDivePlanner.Models;
 using BubblesDivePlanner.Models.Cylinders;
 using BubblesDivePlanner.Models.DiveModels;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace BubblesDivePlanner.Controllers.Json
 {
@@ -16,23 +17,59 @@ namespace BubblesDivePlanner.Controllers.Json
 
         public IDivePlan Deserialise(string expectedDivePlanJson)
         {
+            var converters = AddConverters();
+
             var settings = new JsonSerializerSettings
             {
-                Converters =
-                {
-                    new AbstractConverter<DiveModel, IDiveModel>(),
-                    new AbstractConverter<DivePlan, IDivePlan>(),
-                    new AbstractConverter<Cylinder, ICylinder>(),
-                    new AbstractConverter<GasMixture, IGasMixture>(),
-                    new AbstractConverter<DiveStep, IDiveStep>(),
-                    new AbstractConverter<DiveProfile, IDiveProfile>(),
-                },
+                Converters = converters
             };
 
             var divePlans = JsonConvert.DeserializeObject<List<IDivePlan>>(expectedDivePlanJson, settings);
             var divePlan = divePlans.Last();
 
             return divePlan ?? null;
+        }
+
+        private List<JsonConverter> AddConverters()
+        {
+            List<JsonConverter> jsonConverters = new() {
+                new AbstractConverter<DivePlan, IDivePlan>(),
+                new AbstractConverter<Cylinder, ICylinder>(),
+                new AbstractConverter<GasMixture, IGasMixture>(),
+                new AbstractConverter<DiveStep, IDiveStep>(),
+                new AbstractConverter<DiveProfile, IDiveProfile>(),
+            };
+
+            return DetermineDiveModel(GetDiveModelName(), jsonConverters);
+        }
+
+        private string GetDiveModelName() {
+            // TODO get the dive model name from the file
+            return DiveModelNames.FAKE_USN.ToString();
+        }
+
+        private List<JsonConverter> DetermineDiveModel(string diveModelName, List<JsonConverter> jsonConverters)
+        {
+            if (diveModelName == DiveModelNames.ZHL16.ToString())
+            {
+                jsonConverters.Add(new AbstractConverter<Zhl16Buhlmann, IDiveModel>());
+                
+                return jsonConverters;
+            }
+            else if (diveModelName == DiveModelNames.FAKE_ZHL12.ToString())
+            {
+                jsonConverters.Add(new AbstractConverter<FakeZhl12Buhlmann, IDiveModel>());
+
+                return jsonConverters;
+            }
+            else if (diveModelName == DiveModelNames.FAKE_USN.ToString())
+            {
+                jsonConverters.Add(new AbstractConverter<FakeUsnRev6, IDiveModel>());
+
+                return jsonConverters;
+            }
+
+            return jsonConverters;
         }
     }
 }
