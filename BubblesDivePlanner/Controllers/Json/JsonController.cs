0b8b8303c280d4026c4633dec6268811360af189
Skippy <steelstone3@gmail.com>
2022-12-02 16:34:07 +0000

    Json Controller refactor

--- BubblesDivePlanner/Controllers/Json/JsonController.cs
+++ BubblesDivePlanner/Controllers/Json/JsonController.cs
@@ -41,48 +41,26 @@ namespace BubblesDivePlanner.Controllers.Json
                 new AbstractConverter<DiveProfile, IDiveProfile>(),
             };
 
-            DetermineDiveModel(GetDiveModelName(expectedDivePlanJson), jsonConverters);
+            DetermineDiveModel(expectedDivePlanJson, jsonConverters);
 
             return jsonConverters;
         }
 
-        private string GetDiveModelName(string expectedDivePlanJson)
+        private void DetermineDiveModel(string expectedDivePlanJson, List<JsonConverter> jsonConverters)
         {
             if (expectedDivePlanJson.Contains(DiveModelNames.ZHL16_B.ToString()))
             {
-                return DiveModelNames.ZHL16_B.ToString();
+                jsonConverters.Add(new AbstractConverter<Zhl16Buhlmann, IDiveModel>());
             }
             else if (expectedDivePlanJson.Contains(DiveModelNames.ZHL12.ToString()))
             {
-                return DiveModelNames.ZHL12.ToString();
+                jsonConverters.Add(new AbstractConverter<Zhl12Buhlmann, IDiveModel>());
             }
             else if (expectedDivePlanJson.Contains(DiveModelNames.USN_REVISION_6.ToString()))
             {
-                return DiveModelNames.USN_REVISION_6.ToString();
-            }
-            else if (expectedDivePlanJson.Contains(DiveModelNames.DCAP_MF11F6.ToString()))
-            {
-                return DiveModelNames.DCAP_MF11F6.ToString();
-            }
-
-            return null;
-        }
-
-        private void DetermineDiveModel(string diveModelName, List<JsonConverter> jsonConverters)
-        {
-            if (diveModelName == DiveModelNames.ZHL16_B.ToString())
-            {
-                jsonConverters.Add(new AbstractConverter<Zhl16Buhlmann, IDiveModel>());
-            }
-            else if (diveModelName == DiveModelNames.ZHL12.ToString())
-            {
-                jsonConverters.Add(new AbstractConverter<Zhl12Buhlmann, IDiveModel>());
-            }
-            else if (diveModelName == DiveModelNames.USN_REVISION_6.ToString())
-            {
                 jsonConverters.Add(new AbstractConverter<UsnRevision6, IDiveModel>());
             }
-            else if (diveModelName == DiveModelNames.DCAP_MF11F6.ToString())
+            else if (expectedDivePlanJson.Contains(DiveModelNames.DCAP_MF11F6.ToString()))
             {
                 jsonConverters.Add(new AbstractConverter<DcapMf11f6, IDiveModel>());
             }
